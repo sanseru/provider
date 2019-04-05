a190d2973c2d6b8cc393671a8e176eaf758704b9
Haris Lukman Hakim <sanseru1@yahoo.com>
2019-04-05 09:43:10 +0700

    Perubahan 3

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -60,6 +60,7 @@ function excel()
         header("Pragma: public");
         header("Expires: 0");
         header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+        header("Content-type: application/vnd-ms-excel");
         header("Content-Type: application/force-download");
         header("Content-Type: application/octet-stream");
         header("Content-Type: application/download");
